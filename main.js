b7a9cee0423624d68a2b6afa252e3e335358bf49
Mustafa Alrawi <91510132+codealrawi@users.noreply.github.com>
2023-08-17 13:15:36 +0300

    Update main.js
    added animations

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ const animatedText = document.querySelector('.animated-text');
 const animatedLines = document.querySelectorAll('.animated-line');
 
 function showLinesWithDelay() {
-  animatedLines.forEach((line, index) => {
-    setTimeout(() => {
-      line.style.opacity = '1';
-    }, (index + 1) * 1500);
-  });
-  setTimeout(() => {
-    animatedLines.forEach(line => {
-      line.style.opacity = '0';
+    animatedLines.forEach((line, index) => {
+        setTimeout(() => {
+            line.style.opacity = '1';
+        }, (index + 1) * 1500);
     });
-    showLinesWithDelay();
-  }, (animatedLines.length + 1) * 1500);
+    setTimeout(() => {
+        animatedLines.forEach(line => {
+            line.style.opacity = '0';
+        });
+        showLinesWithDelay();
+    }, (animatedLines.length + 1) * 1500);
 }
 
 setTimeout(() => {
-  animatedText.style.visibility = 'visible';
-  showLinesWithDelay();
+    animatedText.style.visibility = 'visible';
+    showLinesWithDelay();
 }, 2000);
